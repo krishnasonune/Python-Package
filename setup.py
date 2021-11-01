b636ebf379aefda3cad3661de650be739ac9be8c
Krishna <krishnasonune87@gmail.com>
2021-11-01 19:10:53 +0530

    Python-Package

--- setup.py
+++ setup.py
@@ -1,19 +1,21 @@
 from setuptools import setup, find_packages
 
-VERSION = '0.1.2'
+VERSION = '0.1.6'
 DESCRIPTION = 'My first Python package'
 
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
 
 
-# Setting up
 setup(
-    # the name must match the folder name 'verysimplemodule'
+
     name="TrackFit",
     version=VERSION,
     author="krishna sonune",
     author_email="krishnasonune87@gmail.com",
     description=DESCRIPTION,
-    long_description=long,
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     packages=find_packages(),
     install_requires=[],
     keywords=['python', 'first package'],
